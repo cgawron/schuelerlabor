fa30fead659b16baaffa71110a094b63b11472a7
H.Schulte <50828453+dk2jk@users.noreply.github.com>
2021-10-28 18:10:17 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -2,16 +2,23 @@ import ubinascii
 import network
 import time
 from umqttsimple import MQTTClient
+#import BME280
+from BME280_1 import BME280
+from machine import I2C,Pin
 
-
-ssid = 'REPLACE_WITH_YOUR_SSID'
-password = 'REPLACE_WITH_YOUR_PASSWORD'
-mqtt_server = 'broker.hivemq.com'
-#mqqt_server = "18.194.65.151"
+ssid = 'cgawron'
+password = 'Schuelerlabor'
+#ssid = 'deine_ssid'
+#password = 'dein_passwort'
+#mqtt_server = 'broker.hivemq.com'
+mqtt_server = "34.194.225.123"
+CHANNEL_ID    = "1533730"
+WRITE_API_KEY = 'OFSMOZN1COHG6PAV'
+topic_pub = "channels/" + CHANNEL_ID + "/publish/" + WRITE_API_KEY
 
 client_id = ubinascii.hexlify(machine.unique_id())
 topic_sub = 'testtopic/1'
-topic_pub = 'testtopic/1'
+#topic_pub = 'schuelerlabor/egs'
 
 last_message = 0
 message_interval = 5
@@ -47,8 +54,17 @@ def connect_and_subscribe():
 
 client = connect_and_subscribe()
 count = 1
+i2c = I2C(scl=Pin(5), sda=Pin(4), freq=100000)
+bme = BME280(address=0x77, i2c=i2c)
+
 while True:
-    print("Publishing message: " + str(count))
-    client.publish(topic_pub, 'Zaehler ' + str(count))
+    ergebnis = bme.temperature, bme.humidity, bme.pressure
+    print("Publishing message: " + str(ergebnis))
+    payload ="field1="+str(bme.temperature)+\
+            "&field2="+str(bme.humidity)+\
+            "&field3="+str(bme.pressure)
+#     client.publish(topic_pub + '/temperature', str(bme.temperature))
+#     client.publish(topic_pub + '/humidity', str(bme.humidity))
+    client.publish(topic_pub, payload)
     count += 1
-    time.sleep(1)
+    time.sleep(20)
